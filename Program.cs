78de35a6b51114f44378bc3ad0d1f4829bfbe2ef
mm-andrade <marcelo.m.andrade@outlook.com>
2024-01-02 20:08:35 -0300

    :sparkles: feat: Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Space Impact");
+
+            Iphone iphone = new Iphone("987654321", "iPhone 11", "987654321", 64);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Apple Music");
+        }
+    }
+}
